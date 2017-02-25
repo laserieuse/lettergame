9d101ca7ff6d677f65e87b633ea5b25660d1d187
amina <amina@DESKTOP-UPMTKLF>
2017-02-25 13:26:48 +0100

    essaie

--- TP_4A_2017_Letter_Game-master/LetterGame/src/main/java/fr/esiea/unique/binome/name/dictionary/Dictionary.java
+++ TP_4A_2017_Letter_Game-master/LetterGame/src/main/java/fr/esiea/unique/binome/name/dictionary/Dictionary.java
@@ -1,10 +1,10 @@
 package fr.esiea.unique.binome.name.dictionary;
 
-import java.util.Optional;
+import org.junit.runner.manipulation.Filter;
 
-public class Dictionary 
+public abstract class Dictionary implements IDictionary 
 {
-	private Cell origin = new Cell('0');
+	/*private Cell origin = new Cell('0');
 	
 	public void addWord(String w)
 	{
@@ -14,29 +14,70 @@ public class Dictionary
 			currentCell = currentCell.getOrCreate(currentLetter);
 		}
 		
-	}
+	}*/
+	
+	private static Scanner scan;
+	private BufferedReader br;
+
+	public static void main(String[] args) {		
+
+}
 	
 	
 	
 	public boolean isWordValid(String w)
 	{
-		Cell currentCell = origin;
-		for (char currentLetter : w.toCharArray())
-		{
+		/* Cell currentCell = origin;*/
+		/*for (char currentLetter : w.toCharArray())*/
+		
 			/*currentCell = currentCell.get(currentLetter);
 			if (currentCell == null)
 			{
 				return false;
 			}*/
 			
-			Optional<Cell> optional = currentCell.get(currentLetter);
+			/*Optional<Cell> optional = currentCell.get(currentLetter);
 			if (!optional.isPresent())
 			{
 				return false;
 			}
 			
 		} return currentCell.isEndOfWord();
+	}*/
+			try{
+
+				FileReader fr = new Filter("src/main/resources/dico.txt");
+				br = new BufferedReader(fr);
+				String s;
+				String wordDico = null;
+
+				while((s = br.readLine()) != null) {
+					scan = new Scanner(s);
+					while (scan.hasNext()) {
+						wordDico = scan.next().toLowerCase();
+						if(wordDico.equals(w)) {
+							System.out.println("Mot existant");
+							return true;
+						}
+					}
+				}
+			br.close();
+			fr.close();
+			System.out.println("Mot inexistant");
+			return false;
+
+			} catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+			}
+			return false;
 	}
-	
+
+
+
+	@Override
+	public boolean isWord(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
