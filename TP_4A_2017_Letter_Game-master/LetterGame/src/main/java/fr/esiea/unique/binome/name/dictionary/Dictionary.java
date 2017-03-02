e36271c13635359a79ad857f3be1d7545650c1af
amina <amina@DESKTOP-UPMTKLF>
2017-03-02 19:04:27 +0100

    modified

--- TP_4A_2017_Letter_Game-master/LetterGame/src/main/java/fr/esiea/unique/binome/name/dictionary/Dictionary.java
+++ TP_4A_2017_Letter_Game-master/LetterGame/src/main/java/fr/esiea/unique/binome/name/dictionary/Dictionary.java
@@ -2,82 +2,69 @@ package fr.esiea.unique.binome.name.dictionary;
 
 import org.junit.runner.manipulation.Filter;
 
-public abstract class Dictionary implements IDictionary 
-{
-	/*private Cell origin = new Cell('0');
-	
-	public void addWord(String w)
-	{
-		Cell currentCell = origin;
-		for (char currentLetter : w.toCharArray())
-		{
-			currentCell = currentCell.getOrCreate(currentLetter);
+import java.io.BufferedReader;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java.io.InputStream;
+
+import java.io.InputStreamReader;
+
+public class Dictionary  {
+
+	public BufferedReader OuvrirFichier(String motTester) {
+
+		String fichier = "src/main/ressources/dico.txt";
+
+		BufferedReader br = null;
+
+		try {// Ouvrir fichier
+
+			InputStream ips = new FileInputStream(fichier);
+
+			InputStreamReader ipsr = new InputStreamReader(ips);
+
+			br = new BufferedReader(ipsr);
+
 		}
-		
-	}*/
-	
-	private static Scanner scan;
-	private BufferedReader br;
 
-	public static void main(String[] args) {		
+		catch (Exception e) {
 
-}
-	
-	
-	
-	public boolean isWordValid(String w)
-	{
-		/* Cell currentCell = origin;*/
-		/*for (char currentLetter : w.toCharArray())*/
-		
-			/*currentCell = currentCell.get(currentLetter);
-			if (currentCell == null)
-			{
-				return false;
-			}*/
-			
-			/*Optional<Cell> optional = currentCell.get(currentLetter);
-			if (!optional.isPresent())
-			{
-				return false;
-			}
-			
-		} return currentCell.isEndOfWord();
-	}*/
-			try{
+			System.out.println(e.toString());
 
-				FileReader fr = new Filter("src/main/resources/dico.txt");
-				br = new BufferedReader(fr);
-				String s;
-				String wordDico = null;
+		}
 
-				while((s = br.readLine()) != null) {
-					scan = new Scanner(s);
-					while (scan.hasNext()) {
-						wordDico = scan.next().toLowerCase();
-						if(wordDico.equals(w)) {
-							System.out.println("Mot existant");
-							return true;
-						}
-					}
+		return br;
+
+	}
+	 @Override
+
+		public String isWord (String motTest, BufferedReader br) throws IOException {
+
+			String currentLine;
+
+			do {
+
+				currentLine = br.readLine();
+
+				if (currentLine.equals(motTest) || currentLine.equals(null)) {
+
+					// System.out.println("Le mot : " + currentLine + " a ete trouve
+					// dans le dictionnaire");
+
+					br.close();
+
+					return currentLine;
+
 				}
-			br.close();
-			fr.close();
-			System.out.println("Mot inexistant");
-			return false;
+			} while (br.ready());
 
-			} catch (Exception e){//Catch exception if any
-			System.err.println("Error: " + e.getMessage());
-			}
-			return false;
-	}
+			System.out.println("Error Detected.");
 
+			return "";
 
-
-	@Override
-	public boolean isWord(String word) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		}
 	
 }
+
--- TP_4A_2017_Letter_Game-master/LetterGame/src/main/java/fr/esiea/unique/binome/name/dictionary/IDictionary.java
+++ TP_4A_2017_Letter_Game-master/LetterGame/src/main/java/fr/esiea/unique/binome/name/dictionary/IDictionary.java
@@ -1,9 +1,14 @@
 package fr.esiea.unique.binome.name.dictionary;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 /**
  * Created on 16/01/17.
  */
 public interface IDictionary 
 {
     boolean isWord(String word);
+    
+   
 }
